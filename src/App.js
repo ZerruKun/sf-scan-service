f5639edcd580fd7ff897bdd5a154e3d5523a4336
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2023-01-03 14:26:27 +0300

    Footer basic layout.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Bottom/Footer/Footer';
 import Header from './components/Top/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
