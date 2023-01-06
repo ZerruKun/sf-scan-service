6a979af90cb00bb60f424914d1a929949fb45b3b
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2023-01-06 13:31:27 +0300

    WhyWe basic layout.

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SerchService from '../../components/Middle/SerchService/SerchService'
+import WhyWe from "../../components/Middle/WhyWe/WhyWe"
 
 const Landing = () => {
   return (
     <div>
       <SerchService />
+      <WhyWe />
     </div>
   )
 }
